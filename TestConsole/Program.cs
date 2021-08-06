bc4c027e345bb3dd1559516595be78a00f83d05c
ALI <kradunmaker@gmail.com>
2021-08-06 18:24:22 +0500

    Add project files.

---
+++ TestConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using FileSystemVisitor;
+using System;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Finder finder = new Finder();
+            finder.GetFiles(@"C:\Users\User\Desktop\Exeption handling", ".");
+            finder.Started += () => Onstarted();
+            finder.OnStarted();
+            foreach (var item in finder.Files)
+            {
+                Console.WriteLine(item);
+            }
+            finder.Finishied += () => OnFinished();
+            finder.OnFinished();
+        }
+        static void Onstarted()
+        {
+            Console.WriteLine("Process Started!");
+        }
+        static void OnFinished()
+        {
+            Console.WriteLine("Process Started!");
+        }
+    }
+}
